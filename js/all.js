02157530d7091f6707f5e0b087ab99c7fee30fa0
BrianC72 <gx8228@hotmail.com>
2018-12-29 13:23:45 +0800

    upload total file

---
+++ js/all.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log('ready');
+    $('a[data-dismiss="modal"][data-toggle="modal"]').on('click', function(){
+      console.log('click');
+      var target = $(this).data('target');
+      console.log('target', target);
+      $(target).on('shown.bs.modal', function(){
+        $('body').addClass('modal-open')
+      });
+    });
+    
+    
+  });
